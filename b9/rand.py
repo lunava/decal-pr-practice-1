724035e3fb1fa577a070625b7cea41e4bd5cfb89
lunava <lnavarret010@berkeley.edu>
2021-04-18 03:15:22 +0000

    dice rolling WIP

--- b9/rand.py
+++ b9/rand.py
@@ -61,8 +61,10 @@ def roll_dice(iterations, sides):
 
 
     # COMMIT 2: Add dice rolling logic and output dice sum and sequence
+    diceSum = random.randint(1, iterations * 6)
 
-
+    print("{} roll(s) of a {}-sided die resulted in a sum of {}:"
+            .format(iterations, sides, diceSum))
     return
 
 if __name__ == "__main__":
